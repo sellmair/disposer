6ab8ca79ce1fd79165696bc3449375d67f4bd7bc
Sebastian Sellmair <sebastiansellmair@gmail.com>
2018-09-29 14:55:51 +0200

    Add examples to MainActivity

--- app/src/main/java/io/sellmair/rxlifecycle/MainActivity.kt
+++ app/src/main/java/io/sellmair/rxlifecycle/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.sellmair.rxlifecycle
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.reactivex.Observable
@@ -8,10 +9,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        Observable.just(true)
-            .subscribe()
-            .disposeBy(onDestroy)
+
+    @SuppressLint("CheckResult")
+    override fun onStart() {
+        super.onStart()
+
+        lifecycle.disposers.onStop += fetchData()
+            .flatMap(::prepareData)
+            .subscribe(::displayData)
+
+
+        fetchData()
+            .flatMap(::prepareData)
+            .disposeBy(lifecycle.disposers.onStop)
+            .subscribe(::displayData)
+
+
+        fetchData()
+            .flatMap(::prepareData)
+            .subscribe(::displayData)
+            .disposeBy(lifecycle.disposers.onStop)
+
 
     }
+
+    fun displayData(unit: Unit) {
+
+    }
+}
+
+
+fun fetchData(): Observable<Unit> {
+    return Observable.just(Unit)
+}
+
+fun prepareData(unit: Unit): Observable<Unit> {
+    return Observable.just(Unit)
 }
