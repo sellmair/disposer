48cf82151ce2cbe6dec62ea67b491980bdd12fc1
Sebastian Sellmair <sebastiansellmair@gmail.com>
2019-02-03 14:49:41 +0100

    - Add Disposer.createSlave API - Add Dispser.Companion.invoke API (as syntactic sugar) - Add Disposer.create(master: Disposer) API

--- lib/src/main/java/io/sellmair/disposer/Disposer.kt
+++ lib/src/main/java/io/sellmair/disposer/Disposer.kt
@@ -1,3 +1,5 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
 package io.sellmair.disposer
 
 import io.reactivex.*
@@ -7,22 +9,22 @@ import io.sellmair.disposer.internal.LockedDisposer
 
 /**
  * A Disposer is parent of multiple [Disposable] objects that will be disposed
- * once the disposer receives a [dispose] event.
+ * once the disposer receives a [Disposer.dispose] event.
  *
- * Disposers instances can be reused and allow for multiple [dispose] calls.
+ * Disposers instances can be reused and allow for multiple [Disposer.dispose] calls.
  * Be aware, that this fact means, that a [Disposer] will therefore always
  * return true in [isDisposed]
  */
 interface Disposer : Disposable {
 
     /**
-     * Disposer is never disposed, because the [dispose] function can be called
+     * Disposer is never disposed, because the [Disposer.dispose] function can be called
      * multiple times, which will just reset the state of the disposer
      */
     override fun isDisposed(): Boolean = false
 
     /**
-     * @param disposable will be disposed once this disposer receives a [dispose] signal
+     * @param disposable will be disposed once this disposer receives a [Disposer.dispose] signal
      */
     fun add(disposable: Disposable)
 
@@ -32,69 +34,85 @@ interface Disposer : Disposable {
     operator fun plusAssign(disposable: Disposable)
 
     /**
-     * Will dispose the upstream once this disposer receives a [dispose] signal
+     * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
      */
     fun <T> bind(observable: Observable<T>): Observable<T>
 
     /**
-     * Will dispose the upstream once this disposer receives a [dispose] signal
+     * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
      */
     fun <T> bind(flowable: Flowable<T>): Flowable<T>
 
     /**
-     * Will dispose the upstream once this disposer receives a [dispose] signal
+     * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
      */
     fun <T> bind(single: Single<T>): Single<T>
 
     /**
-     * Will dispose the upstream once this disposer receives a [dispose] signal
+     * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
      */
     fun <T> bind(maybe: Maybe<T>): Maybe<T>
 
     /**
-     * Will dispose the upstream once this disposer receives a [dispose] signal
+     * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
      */
     fun bind(completable: Completable): Completable
 
 
     companion object {
-        fun create(): Disposer = LockedDisposer()
+
+        /**
+         *  Syntactically sweeter version of [create]
+         *  
+         *  @see create
+         */
+        operator fun invoke(master: Disposer? = null) = this.create(master)
+
+        /**
+         * Creates a new thread safe [Disposer] instance.
+         * @param master: The newly created disposer instance will receive all all
+         * [Disposer.dispose] events from this master disposer and will also dispose on
+         * each of those events.
+         */
+        fun create(master: Disposer? = null): Disposer {
+            return LockedDisposer().apply { master?.add(this) }
+        }
     }
 }
 
 
 /**
- * Will dispose the upstream once this disposer receives a [dispose] signal
+ * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
  * @see Disposer.bind
  */
 fun <T> Observable<T>.disposeBy(disposer: Disposer): Observable<T> = disposer.bind(this)
 
 /**
- * Will dispose the upstream once this disposer receives a [dispose] signal
+ * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
  * @see Disposer.bind
  */
 fun <T> Flowable<T>.disposeBy(disposer: Disposer): Flowable<T> = disposer.bind(this)
 
 /**
- * Will dispose the upstream once this disposer receives a [dispose] signal
+ * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
  * @see Disposer.bind
  */
 fun <T> Single<T>.disposeBy(disposer: Disposer): Single<T> = disposer.bind(this)
 
 /**
- * Will dispose the upstream once this disposer receives a [dispose] signal
+ * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
  * @see Disposer.bind
  */
 fun <T> Maybe<T>.disposeBy(disposer: Disposer): Maybe<T> = disposer.bind(this)
 
 /**
- * Will dispose the upstream once this disposer receives a [dispose] signal
+ * Will dispose the upstream once this disposer receives a [Disposer.dispose] signal
  * @see Disposer.bind
  */
 fun Completable.disposeBy(disposer: Disposer): Completable = disposer.bind(this)
 
 /**
- * Will dispose once this disposer receives a [dispose] signal
+ * Will Disposer.dispose once this disposer receives a [Disposer.dispose] signal
  * @see Disposer.bind
  */
 fun Disposable.disposeBy(disposer: Disposer) = this.apply { disposer.add(this) }
--- lib/src/main/java/io/sellmair/disposer/internal/LockedDisposer.kt
+++ lib/src/main/java/io/sellmair/disposer/internal/LockedDisposer.kt
@@ -15,11 +15,23 @@ INTERNAL API
 
 internal class LockedDisposer : Disposer {
 
-    private val lock = ReentrantLock()
+    /**
+     * Lock used to synchronize the access of [disposables]
+     */
+    private val disposablesLock = ReentrantLock()
 
+    /**
+     * All currently held disposables that are waiting for the next [dispose] signal.
+     * Synchronized via [disposablesLock]
+     */
     private val disposables = mutableListOf<Disposable>()
 
-    override fun add(disposable: Disposable): Unit = lock.withLock {
+    /**
+     * Lock used to synchronize the [dispose] function
+     */
+    private val disposeLock = ReentrantLock()
+
+    override fun add(disposable: Disposable): Unit = disposablesLock.withLock {
         disposables.add(disposable)
     }
 
@@ -49,10 +61,23 @@ internal class LockedDisposer : Disposer {
         return completable.doOnSubscribe(this::add)
     }
 
-    override fun dispose() {
-        val disposables = lock.withLock(disposables::poll)
-        for (disposable in disposables) {
-            disposable.dispose()
+    override fun dispose(): Unit = disposeLock.withLock {
+        /* Get all currently registered disposables */
+        val toDispose = disposablesLock.withLock(disposables::poll)
+
+        /*
+        Dispose all disposables and collect all that are still returning 'not disposed'.
+        This objects signal that they might want to receive further dispose events
+        e.g. Disposer objects
+         */
+        val notDisposed = toDispose.asSequence()
+            .onEach(Disposable::dispose)
+            .filterNot(Disposable::isDisposed)
+
+
+        /* Re-Add all 'not disposed objects to the list of disposables */
+        disposeLock.withLock {
+            this.disposables.addAll(notDisposed)
         }
     }
 }
